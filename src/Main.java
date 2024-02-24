aaff73cd638b3b10579f3b9f0b409f729038dac3
fern4nandomartins <fernando.gouvea@gmail.com>
2024-02-24 10:57:34 -0300

    first

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,39 @@
-import java.util.Scanner;
 import java.io.*;
+import javax.swing.JOptionPane;
+
 public class Main {
     static String user;
     static String password;
+    public static void main(String[] args) {
+        int i = JOptionPane.showConfirmDialog(null, "create new account?");
+        if (i == 2) {
+            System.exit(0);
+        }
+        user = JOptionPane.showInputDialog("Type username");
+        password = JOptionPane.showInputDialog("Type password");
+//        db.dbLog();
+    }
+}
+
+class db {
+    public static void server() {
+
+    }
+    private static final String dbfile = "data.txt";
+    public static boolean dbSign(String user, String passwd) {
+        File file = new File(dbfile);
+        return false;
+    }
+    public static boolean dbLog(String user, String passwd) {
+        File file = new File(dbfile);
+        return true;
+    }
+    private static String readFrom(String dbName) {
+        return "";
+    }
+    private static void writeTo(String dbName, String text) {
+
+    }
 
     public static String readData() {
         try {
@@ -17,30 +48,4 @@ public class Main {
         }
         return "blaaa";
     }
-
-
-
-    public static void main(String[] args) {
-        Scanner scn = new Scanner(System.in);
-        System.out.println("type in user name: ");
-        user = scn.nextLine();
-        System.out.println("type in password: ");
-        password = scn.nextLine();
-        readData();
-
-        System.out.println("you logged in successfully.");
-        db.po();
-    }
-}
-
-class db {
-    private static String dbfile;
-    private static String user;
-    public static String openFile(String filename) {
-        dbfile = filename;
-        File file = new File(dbfile);
-        return "asasa";
-    }
-    public static String readFrom() {}
-    public static String writeTo() {}
 }
